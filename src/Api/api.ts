f15921e81dd8019cd89fbe3c1eb7f4763cf8833b
pelicanStd <pelican8118@gmail.com>
2023-08-02 13:56:30 +0900

    feat: Api 작성

--- src/Api/api.ts
+++ src/Api/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const ApiHttp = axios.create({
-  baseURL: "src/Api/data/"
+  baseURL: "http://52.78.200.157/api",
+  headers:{
+  }
 });
 
 // local storage, session control api 추가 필요
@@ -13,7 +15,6 @@ export async function getMyPage() {
     return res.data;
   } catch (error) {
     console.log(error);
-    throw error;
   }
 }
 
@@ -23,6 +24,44 @@ export async function getUser() {
     return res.data;
   } catch (error) {
     console.error('유저 data를 받아 오는데 실패 하였습니다.')
-    throw error;
   }
 }
+
+// 로그인 api
+export async function postLogin(data:LoginType) {
+  try{
+    const res = await ApiHttp.post('login',data)
+    return res.data;
+  } catch (error) {
+    console.error('로그인에 실패 하였습니다.')
+  }
+}
+
+// 회원등록 api
+export async function postRegister(data:User) {
+  try{
+    const res = await ApiHttp.post('/register',data)
+    return res.data;
+  } catch (error) {
+    console.error('등록에 실패 하였습니다.')
+  }
+}
+
+export async function getMainCalendar() {
+  try{
+    const res = await ApiHttp.get('/main')
+    return res.data;
+  } catch (error) {
+    console.error('data를 가져오는데 실패 하였습니다.')
+  }
+}
+
+export async function postAnnual(data:AnnualType|DutyType) {
+  try{
+    const res = await ApiHttp.post('/annual',data)
+    return res.data;
+  } catch (error){
+    console.log('연차/당직 등록에 실패하였습니다.')
+  }
+}
+
